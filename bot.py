b0154d0313ed7b0143f5d48dd0ce9c134c8de7aa
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-13 16:49:47 +0500

    done

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from telegram.ext import (
 from callback_functions import (
     start, brands,
     send_products, send_product_detail,
+    remove_cart, add_cart
 )
 from db import DB
 
@@ -24,6 +25,8 @@ def main():
     dispatcher.add_handler(handler=CallbackQueryHandler(brands, pattern="mavjud brendlar"))
     dispatcher.add_handler(handler=CallbackQueryHandler(send_products, pattern="brend:"))
     dispatcher.add_handler(handler=CallbackQueryHandler(send_product_detail, pattern="product:"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(remove_cart, pattern="remove"))
+    dispatcher.add_handler(handler=CallbackQueryHandler(add_cart, pattern="add-cart:"))
 
     updater.start_polling()
     updater.idle()
--- callback_functions.py
+++ callback_functions.py
@@ -11,6 +11,7 @@ db = DB(file_name='db.json')
 
 def start(update: Update, context: CallbackContext) -> None:
    
+    db.add_user(chat_id=update.message.chat.id, first_name=update.message.chat.first_name, last_name=update.message.chat.last_name, username=update.message.chat.username)
 
     btn1= InlineKeyboardButton(text = "mavjud brendlar", callback_data="mavjud brendlar")
     
@@ -29,7 +30,7 @@ def start(update: Update, context: CallbackContext) -> None:
 def brands(update: Update, context: CallbackContext):
     inline_keyboards = []
     for brend in db.get_brends():
-        btn1 = InlineKeyboardButton(text=brend , callback_data=f"brend:{brend}")
+        btn1 = InlineKeyboardButton(text=brend , callback_data="brend:"+brend)
         inline_keyboards.append([btn1])
 
     update.callback_query.message.reply_text(
@@ -52,11 +53,24 @@ def send_products(update: Update, context: CallbackContext):
 
 def send_product_detail(update: Update, context: CallbackContext):
     product_id = int(update.callback_query.data.split(":")[1])
+    add_cart_btn = InlineKeyboardButton(text="savatga qo'shish", callback_data=f"add-cart:{product_id}")
+    close_btn = InlineKeyboardButton(text="yopish", callback_data="remove")
     product = db.get_phones_by_id(product_id)
     update.callback_query.message.reply_photo(
         photo=product['image'],
-        caption = f"bizda {product['name']} mahsuloti quyidagilar ⏬\n\nbrendi: {product['brend']}\nrang: {product['color']}\nram: {product['ram']}\nxotira: {product['memory']}\nnarxi: {product['price']}"
-        # reply_markup=InlineKeyboardMarkup(inline_keyboard=inline_keyboards)
+        caption = f"bizda {product['name']} mahsuloti quyidagilar ⏬\n\nbrendi: {product['brend']}\nrang: {product['color']}\nram: {product['ram']}\nxotira: {product['memory']}\nnarxi: {product['price']}",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[add_cart_btn, close_btn]])
     )
 
-    
+def remove_cart(update: Update, context: CallbackContext):
+    update.callback_query.message.delete()
+
+
+def add_cart(update: Update, context: CallbackContext):
+    chat_id = update.callback_query.message.chat.id
+    product_id = update.callback_query.data.split(':')[1]  # 'add-dart:2' -> ['add-dart', '2'] -> 2
+
+    db.add_cart(chat_id, product_id)
+    update.callback_query.message.reply_text(
+        text="savatga qo'shildi"
+    )
